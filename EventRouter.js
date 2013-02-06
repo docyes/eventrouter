af504f60b7de0ad9d88f869ea13fbac0c8931cdf
Carl S. Yestrau <carl@featureblend.com>
2013-02-06 15:39:53 -0800

    spacing fixes

--- EventRouter.js
+++ EventRouter.js
@@ -1,46 +1,46 @@
 function EventRouter(options) {
-	options || (options = {});
-	this.id = EventRouter.uniqueId();
-	this.$el = (options.$el) ? options.$el : $('body');
-	this.context = options.context || null;
+    options || (options = {});
+    this.id = EventRouter.uniqueId();
+    this.$el = (options.$el) ? options.$el : $('body');
+    this.context = options.context || null;
 }
 EventRouter.prototype = {
-	splitter: /^(\S+)\s*(.*)$/;
-	route: function(routes) {
-		if (!events) {
-			return;
-		}
-		this.unroute();
-		for (var key in events) {
-			var method = events[key];
-			if (typeof method !== 'function') {
-				continue;
-			}
-			var match = key.match(this.splitter);
-			var eventName = match[1];
-			var selector = match[2];
-			if (this.context) {
-				method = EventRouter.bind(method, this.context);
-			}
-			eventName += '.EventRouter' + this.id;
-	        if (selector === '') {
-	        	this.$el.on(eventName, method);
-		    } else {
-		    	this.$el.on(eventName, selector, method);
-		    }
-		}
-	},
-	unroute: function(events) {
-		this.$el.off('.EventRouter' + this.id);
-	}	
+    splitter: /^(\S+)\s*(.*)$/;
+    route: function(routes) {
+        if (!events) {
+            return;
+        }
+        this.unroute();
+        for (var key in events) {
+            var method = events[key];
+            if (typeof method !== 'function') {
+                continue;
+            }
+            var match = key.match(this.splitter);
+            var eventName = match[1];
+            var selector = match[2];
+            if (this.context) {
+                method = EventRouter.bind(method, this.context);
+            }
+            eventName += '.EventRouter' + this.id;
+            if (selector === '') {
+                this.$el.on(eventName, method);
+            } else {
+                this.$el.on(eventName, selector, method);
+            }
+        }
+    },
+    unroute: function(events) {
+        this.$el.off('.EventRouter' + this.id);
+    }
 };
 EventRouter.idCounter = 0;
 EventRouter.uniqueId = function() {
     return ++EventRouter.idCounter;
 };
 EventRouter.bind = function(func, context) {
-	var nativeBind = Function.prototype.bind;
-	var slice = Array.prototype.slice;
+    var nativeBind = Function.prototype.bind;
+    var slice = Array.prototype.slice;
     if (func.bind === nativeBind && nativeBind) return nativeBind.apply(func, slice.call(arguments, 1));
     var args = slice.call(arguments, 2);
     return function() {
